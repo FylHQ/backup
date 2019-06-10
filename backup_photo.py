aa37e4370e4fa4b0a9c9894731f642a5f9531601
Andrey Klyukvin <aklukvin@gmail.com>
2019-06-10 14:37:45 +0300

    dry run + spaces in dir names

--- backup_photo.py
+++ backup_photo.py
@@ -6,26 +6,27 @@ datePattern = re.compile('.*(20\d{2})-?(\d{2})-?\d{2}.*')
 
 def run(*args):
    pcs = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-   if pcs.returncode != 0:
-      for line in pcs.stdout.splitlines():
+   for line in pcs.stdout.splitlines():
+      if all(line.find(x) == -1 for x in ('cannot listen to port', 'Address already in use', 'Could not request local forwarding')):
          print(line)
+   if pcs.returncode != 0:
       return False
    else:
-      for line in pcs.stdout.splitlines():
-         if all(line.find(x) == -1 for x in ('cannot listen to port', 'Address already in use', 'Could not request local forwarding')):
-            print(line)
       return True
 
 def mkdir(path, host=None):
    if host:
-      return run('ssh', host, f'mkdir -p {path}')
+      return run('ssh', host, f'mkdir -p "{path}"')
    else:
       return run('mkdir', '-p', f'{path}')
    
-def rsync(inputPath, fileList, outputPath, host=None):
+def rsync(inputPath, fileList, outputPath, dryRun=False, host=None):
    args = ["rsync", "-avzz", "--files-from", fileList, inputPath]
+   if dryRun:
+      args.append('-n')
+   
    if host:
-      args.append(f'{host}:{outputPath}')
+      args.append(f'{host}:"{outputPath}"')
    else:
       args.append(f'{outputPath}')
 
@@ -60,7 +61,7 @@ def createYearDirs(files, outputPath, host=None):
    
    return True
 
-def backup(inputPath, outputPath, host=None):
+def backup(inputPath, outputPath, dryRun=False, host=None):
    (files, orphans) = sortFiles(inputPath)
 
    if not createYearDirs(files, outputPath, host):
@@ -74,7 +75,7 @@ def backup(inputPath, outputPath, host=None):
                f.writelines(files[directory][year][month])
 
             monthOut = f'{outputPath}/{year}/{month}/'
-            if not rsync(directory, filesList.name, monthOut, host):
+            if not rsync(directory, filesList.name, monthOut, dryRun, host):
                print(f'Cannot rsync directory {directory} to {monthOut}')
                return False
             else:
@@ -84,7 +85,7 @@ def backup(inputPath, outputPath, host=None):
       with open(filesList.name, 'w') as f:
          f.writelines(orphans)
 
-      if not rsync(inputPath, filesList.name, outputPath, host):
+      if not rsync(inputPath, filesList.name, outputPath, dryRun, host):
          print(f'Cannot rsync orphans from directory {inputPath} to {outputPath}')
          return False
       else:
@@ -93,11 +94,15 @@ def backup(inputPath, outputPath, host=None):
    return True
 
 if __name__ == "__main__":
-   parser = argparse.ArgumentParser(description='Backup by year and month')
+   parser = argparse.ArgumentParser(description='Backup by year and month', add_help=False)
+   parser.add_argument('--help', action='help', default=argparse.SUPPRESS, help=argparse._('show this help message and exit'))
    parser.add_argument('input')
-   parser.add_argument('--host', help='Output host')
+   parser.add_argument('--host', '-h', help='Output host')
+   parser.add_argument('--dry-run', '-n', action="store_true", help='Perform a trial run with no changes made')
    parser.add_argument('output')
    args = parser.parse_args()
+
+   print(args)
 
    inputPath = os.path.realpath(os.path.expanduser(args.input))
    print(f'Input folder: {inputPath}')
@@ -105,10 +110,13 @@ if __name__ == "__main__":
    outputPath = os.path.realpath(os.path.expanduser(args.output))
    print(f'Output folder: {outputPath}')
 
+   if args.dry_run:
+      print('Performing dry run')
+
    if args.host:
       print(f'Host: {args.host}')
    
-   if backup(inputPath, outputPath, args.host):
+   if backup(inputPath, outputPath, args.dry_run, args.host):
       print('Backup completed successfully')
    else:
       print('Some error has occured')
